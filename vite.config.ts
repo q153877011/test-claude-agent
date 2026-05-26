6a4cb6eab24c2ad506dd658ef47c3ab30939ac86
wenyiqing <wenyiqing@tencent.com>
2026-05-26 19:37:56 +0800

    chore: address open source audit fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  // @tencent/edgeone is a backend/deploy dependency, frontend does not need pre-optimization
-  optimizeDeps: {
-    exclude: ['@tencent/edgeone'],
-  },
-  server: {
-    port: 5173,
-    headers: {
-      'Connection': 'keep-alive',
-    },
-  },
+  plugins: [react() as PluginOption]
 })
